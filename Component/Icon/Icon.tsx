1a0119700f9c180b31353fd1d329e9be81fb9e46
EhsanFalahian <ehsanfalahian.work@gmail.com>
2022-06-08 05:15:59 -0700

    init commit

--- Component/Icon/Icon.tsx
+++ Component/Icon/Icon.tsx
@@ -1,8 +1,9 @@
 import  { Ionicons } from '@expo/vector-icons'
+import { ReactNode } from 'react'
 import * as styled from './Style'
 
 
-type props = {
+type props ={
     name ?: any,
     size ?: number,
     color?: string
@@ -10,9 +11,11 @@ type props = {
 const Icon = (prop : props):JSX.Element =>{
     const { name , size , color} = prop
     return(
+        <>
         <styled.container>
         <Ionicons name={name} size={size} color={color} />
         </styled.container>
+        </>
     )
 }
 
